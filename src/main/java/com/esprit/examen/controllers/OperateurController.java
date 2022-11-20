c9803cc3baeca8351fc89c6375a7bc773eead687
atefmiladi <atefmiladi8@gmail.com>
2022-11-20 11:56:23 -0800

    fix code smell vulnerabilities in OperateurRestController class

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -1,13 +1,21 @@
 package com.esprit.examen.controllers;
 
 import java.util.List;
+import java.util.Set;
+
+import javax.persistence.OneToMany;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Operateur;
 import com.esprit.examen.services.IOperateurService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.Api;
+import lombok.Getter;
+import lombok.Setter;
 
 @RestController
 @Api(tags = "Gestion des opérateurs")
@@ -18,42 +26,69 @@ public class OperateurController {
 	@Autowired
 	IOperateurService operateurService;
 	
-	// http://localhost:8089/SpringMVC/operateur/retrieve-all-operateurs
+	
 	@GetMapping("/retrieve-all-operateurs")
 	@ResponseBody
 	public List<Operateur> getOperateurs() {
-		List<Operateur> list = operateurService.retrieveAllOperateurs();
-		return list;
+		return operateurService.retrieveAllOperateurs();
+	
 	}
 
-	// http://localhost:8089/SpringMVC/operateur/retrieve-operateur/8
+	
 	@GetMapping("/retrieve-operateur/{operateur-id}")
 	@ResponseBody
 	public Operateur retrieveOperateur(@PathVariable("operateur-id") Long operateurId) {
 		return operateurService.retrieveOperateur(operateurId);
 	}
 
-	// http://localhost:8089/SpringMVC/operateur/add-operateur
+	
 	@PostMapping("/add-operateur")
 	@ResponseBody
-	public Operateur addOperateur(@RequestBody Operateur op) {
-		Operateur operateur = operateurService.addOperateur(op);
-		return operateur;
+	public Operateur addOperateur(@RequestBody OperateurModel operateurModel) {
+		Operateur operateur = new Operateur();
+		operateur.setNom(operateurModel.getNom());
+		operateur.setPrenom(operateurModel.getPrenom());
+		operateur.setPassword(operateurModel.getPassword());
+		operateur.setFactures(operateurModel.getFactures());
+		return operateurService.addOperateur(operateur);
 	}
 
-	// http://localhost:8089/SpringMVC/operateur/remove-operateur/{operateur-id}
 	@DeleteMapping("/remove-operateur/{operateur-id}")
 	@ResponseBody
 	public void removeOperateur(@PathVariable("operateur-id") Long operateurId) {
 		operateurService.deleteOperateur(operateurId);
 	}
 
-	// http://localhost:8089/SpringMVC/operateur/modify-operateur
+	
 	@PutMapping("/modify-operateur")
 	@ResponseBody
-	public Operateur modifyOperateur(@RequestBody Operateur operateur) {
+	public Operateur modifyOperateur(@RequestBody OperateurModel operateurModel) {
+	    Operateur operateur = new Operateur();
+        operateur.setNom(operateurModel.getNom());
+        operateur.setPrenom(operateurModel.getPrenom());
+        operateur.setPassword(operateurModel.getPassword());
+        operateur.setFactures(operateurModel.getFactures());
 		return operateurService.updateOperateur(operateur);
 	}
 
 	
 }
+
+@Getter
+@Setter
+class OperateurModel{
+    
+    private Long idOperateur;
+    private String nom;
+    private String prenom;
+    
+    private String password;
+    @OneToMany
+    @JsonIgnore
+    private Set<Facture> factures;
+    
+}
+
+
+
+
